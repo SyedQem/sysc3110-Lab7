8afc5f572de6ec679f6d4de21382531724aa540e
Qurb E Muhammad Syed <qurbemuhammadsyed@cmail.carleton.ca>
2024-11-05 16:37:39 -0500

    Defined ResidentialSite class, and added overloaded constructor along with methods

--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,2 +1,21 @@
 public class ResidentialSite extends Site {
+
+    private double _units;
+    private double _rate;
+
+    ResidentialSite(double units, double rate) {
+        this._units = units;
+        this._rate = rate;
+    }
+
+    @Override
+    double getBaseAmount() {
+        return _units * _rate;
+    }
+
+    @Override
+    double getTaxAmount() {
+        return getBaseAmount() * Site.TAX_RATE;
+    }
+
 }
