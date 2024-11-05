4af227757d07a12af7fe7e2bcdeaaa040316d7e5
Qurb E Muhammad Syed <qurbemuhammadsyed@cmail.carleton.ca>
2024-11-05 16:39:36 -0500

    Added links between Site, Residential Site and Lifeline Site

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,2 +1,21 @@
-public class LifelineSite {
+public class LifelineSite extends Site {
+
+    private double _units;
+    private double _rate;
+
+    LifelineSite(double units, double rate) {
+        this._units = units;
+        this._rate = rate;
+    }
+
+    @Override
+    double getBaseAmount() {
+        return _units * _rate * 0.5;
+    }
+
+    @Override
+    double getTaxAmount() {
+        return getBaseAmount() * Site.TAX_RATE * 0.2;
+    }
+
 }
--- src/Site.java
+++ src/Site.java
@@ -1,2 +1,26 @@
 public class Site {
+
+
+    static final double TAX_RATE = 0.13;
+
+    double getBillableAmount() {
+        return getBaseAmount() + getTaxAmount();
+    }
+    double getBaseAmount() {
+        return 0; // This will be overridden by subclasses
+    }
+
+    double getTaxAmount() {
+        return 0; // This will be overridden by subclasses
+    }
+
+    public static void main(String[] args) {
+        ResidentialSite resSite = new ResidentialSite(10, 10);
+        LifelineSite lifeLineSite = new LifelineSite(10, 10);
+
+        System.out.println("Billable amount for residential site is: "+ resSite.getBillableAmount());
+        System.out.println("Billable amount for lifeline site is: "+ lifeLineSite.getBillableAmount());
+        System.out.println("Same results even after refactoring!");
+    }
 }
+
